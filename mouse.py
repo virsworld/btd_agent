964dc11fd4c874dc4eeb166a08a3428362a1520c
virsworld <virpatel71@gmail.com>
2026-01-21 00:23:25 -0500

    cleaned up test file and updated gitignore

--- mouse.py
+++ mouse.py
@@ -1,14 +1,17 @@
-import pyautogui, sys
-print('Press Ctrl-C to quit.')
+import pyautogui
+
+print("Press Ctrl-C to quit.")
+
+
 def test():
     try:
         while True:
             x, y = pyautogui.position()
-            positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-            print(positionStr, end='')
-            print('\b' * len(positionStr), end='', flush=True)
+            positionStr = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
+            print(positionStr, end="\r")
     except KeyboardInterrupt:
-        print('\n')
+        print("\n")
+
 
 if __name__ == "__main__":
     test()
