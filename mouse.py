5f36f3d721b896058bf67c49d858e2561b508d2d
virsworld <virpatel71@gmail.com>
2026-01-22 19:32:53 -0500

    started uv project and finished place_monkey function

--- mouse.py
+++ mouse.py
@@ -1,7 +1,22 @@
 import pyautogui
+from pyautogui import ImageNotFoundException
+import time
+from pathlib import Path
 
-print("Press Ctrl-C to quit.")
+SCRIPT_DIR = Path(__file__).parent
 
+def place_monkey(type: str, location: tuple[int, int]) -> None:
+    time.sleep(3) # for testing
+    try:
+        x, y = pyautogui.locateCenterOnScreen(f"{SCRIPT_DIR}/monkeys/{type}.png", grayscale=False, confidence=0.3)
+        print(f"Found monkey {type} at: ", (int(x), int(y)))
+        pyautogui.click(x, y)
+        pyautogui.click(location[0], location[1])
+        print(f"Placed monkey {type} at: ", location)
+    except ImageNotFoundException as e:
+        print("Error finding monkey: ", e)
+    except Exception as e:
+        print("Error placing monkey: ", str(e))
 
 def test():
     try:
@@ -12,6 +27,7 @@ def test():
     except KeyboardInterrupt:
         print("\n")
 
-
 if __name__ == "__main__":
-    test()
+    place_monkey("dart_mky", (1920 / 2, 1080 / 2))
+
+#TODO: make functions to place monkeys
